33f92ca40db8ecef0a7751aac17acb084ad4f49a
Al Romano <alexander@virtuallycreative.ca>
2020-10-03 17:31:08 -0400

    👌 IMPROVE: Copy updates, un-grouped URLS

--- index.js
+++ index.js
@@ -3,17 +3,18 @@
 const pkgJSON = require('./package.json');
 const welcome = require('cli-welcome');
 const chalk = require('chalk');
-const brand = hex('#09c').bold.inverse;
-const twitterClr = hex('#1da1f2').bold.inverse;
-const githubClr = hex('#6cc644').bold.inverse;
-const linkedinClr = hex('#8d6cab').bold.inverse;
-const dim = dim;
+const italic = chalk.italic;
+const brand = chalk.hex('#09c').bold.inverse;
+const twitterClr = chalk.hex('#1da1f2').bold.inverse;
+const githubClr = chalk.hex('#6cc644').bold.inverse;
+const linkedinClr = chalk.hex('#8d6cab').bold.inverse;
+const dim = chalk.dim;
 
 welcome({
   title: `Al Romano`,
   version: pkgJSON.version,
   description: pkgJSON.description,
-  bgColor: `#FADC00`,
+  bgColor: `#09c`,
   color: `#000000`,
   bold: true,
   clear: true,
@@ -29,7 +30,10 @@ ${italic(`
   `
 )}
 
+:: URLS ::
 ${brand(` Website `)} - ${dim(`https://virtuallycreative.ca`)}
+
+:: Social ::
 ${twitterClr(` Twitter `)} - ${dim(`https://twitter.com/vip3rousmango`)}
 ${linkedinClr(` LinkedIn `)} - ${dim(`https://www.linkedin.com/in/alromano/`)}
 ${githubClr(` GitHub `)} - ${dim(`https://github.com/vip3rousmango`)}
