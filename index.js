be648e2522783891f89a7ead9943774687193a8b
Al Romano <alexander@virtuallycreative.ca>
2020-10-03 16:29:54 -0400

    👌 IMPROVE: Replaced clear console with cli welcome

--- index.js
+++ index.js
@@ -1,8 +1,24 @@
 #!/usr/bin/env node
-const clearConsole = require('clear-any-console');
 
-// Clear the console :: cross-platform friendly
-clearConsole();
+const pkgJSON = require('./package.json');
+const welcome = require('cli-welcome');
+
+welcome({
+  title: pkgJSON.name,
+  tagline: `Dream. Design. Develop. Deliver.`,
+  version: pkgJSON.version,
+  description: pkgJSON.description,
+  bgColor: `#FADC00`,
+  color: `#000000`,
+  bold: true,
+  clear: true,
+})
+
+console.log(`
+  NAME: ${pkgJSON.name}
+  VERSION: ${pkgJSON.version}
+  DESCRIPTION: ${pkgJSON.description}
+`)
 
 console.log(`
 
