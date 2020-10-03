ff83c8a63faf19b2c95717b9acde1b9897b4efce
Al Romano <alexander@virtuallycreative.ca>
2020-10-03 16:38:21 -0400

    👌 IMPROVE: copy slimming

--- index.js
+++ index.js
@@ -24,16 +24,11 @@ console.log(`
 
 Hi, I'm Al Romano.
 
-I'm a Web Stack Developer with a knack for web analytics, data visualization and accessable dashboards.
+A "Web Stack" Developer with a knack for web analytics, data visualization and accessable dashboards.
+
 I'm a firm believer of "user experience first" and HTML First Prototyping approaches when
 applying best practice standards for web development and passonate about includsive development.
 
-Starting out as a PHP (LAMP) developer (you guessed it, WordPress...) I've recently taken
-an interest in Information Dashboard design and data visualization with a focus on web accesability using JavaScript
-as well as becoming more proficient using Node.js both for pipeline and for building applications (MExN/CLIs).
-
-I have worked for international ad agencies like Saatchi & Saatchi and Upcoming Media,
-local bioinformatics software Cyclica and am currently helping the cause at SickKids Foundation.
 
 Twitter - https://twitter.com/vip3rousmango
 LinkedIn - https://www.linkedin.com/in/alromano/
