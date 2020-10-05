431797178dc6d33fdaa59ee6125173c2a921e486
Al Romano <alexander@virtuallycreative.ca>
2020-10-05 11:51:38 -0400

    👌 IMPROVE: Tightend up copy and added punchline CLI

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ const brand = chalk.hex('#09c').bold.inverse;
 const twitterClr = chalk.hex('#1da1f2').bold.inverse;
 const githubClr = chalk.hex('#6cc644').bold.inverse;
 const linkedinClr = chalk.hex('#8d6cab').bold.inverse;
+const npxCMD = chalk.hex('#FFFFFF').inverse;
 const dim = chalk.dim;
 
 // Alerts
@@ -29,28 +30,24 @@ welcome({
   clear: true,
 })
 
-console.log(`
-${italic(`
+console.log(`${italic(`
   Front-End Dev with a JAMStack Focus with a knack for web analytics, data visualization and a11y development practices.
-  ---
-  I fell in love with JAM-Stack and have no regrets.
-  ---
+  Started with the LAMP-stack, but fell in love with JAM-Stack and haven't looked back since.
   Dad, Husband, Tech Blogger, Airsoft Fanatic, and I try to Sous Vide everything.
   `
 )}
 
+:: CLIs ::
+${brand(` Punchline `)} -> ${npxCMD(`npx @vip3rousmango/punchline`)} -> ${dim(`https://github.com/vip3rousmango/punchline`)}
+
 :: URLS ::
 ${brand(` Website `)} - ${dim(`https://virtuallycreative.ca`)}
 
-:: Social ::
+:: SOCIAL ::
 ${twitterClr(` Twitter `)} - ${dim(`https://twitter.com/vip3rousmango`)}
 ${linkedinClr(` LinkedIn `)} - ${dim(`https://www.linkedin.com/in/alromano/`)}
 ${githubClr(` GitHub `)} - ${dim(`https://github.com/vip3rousmango`)}
-`);
 
-console.log(`
-  ${sym.success} ${success(' SUCCESS ->')} Thank you for downloading my CLI.
-  ${sym.warning} ${warning(' WARNING ->')} Currently, I'm not actively persuing clients.
-  ${sym.info} ${info(' INFO ->')} For more info, please don't hesitate to reach out!
-  ${sym.error} ${error(' ERROR ->')} Stay hungry. Be humble.
+${sym.success} ${success(' SUCCESS ->')} Thank you for downloading my CLI.
+${sym.info} ${info(' INFO -> ')} Stay hungry. Be humble.
 `);
